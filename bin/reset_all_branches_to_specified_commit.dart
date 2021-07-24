a4eb0ae16bec20e860fed2ef76fc349c19b0ad2c
Banee Ishaque K <baneeishaque@gmail.com>
2021-07-25 02:30:47 +0530

    Template Init

---
+++ bin/reset_all_branches_to_specified_commit.dart
@@ -0,0 +1,5 @@
+import 'package:reset_all_branches_to_specified_commit/reset_all_branches_to_specified_commit.dart' as reset_all_branches_to_specified_commit;
+
+void main(List<String> arguments) {
+  print('Hello world: ${reset_all_branches_to_specified_commit.calculate()}!');
+}
---
+++ test/reset_all_branches_to_specified_commit_test.dart
@@ -0,0 +1,8 @@
+import 'package:reset_all_branches_to_specified_commit/reset_all_branches_to_specified_commit.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
