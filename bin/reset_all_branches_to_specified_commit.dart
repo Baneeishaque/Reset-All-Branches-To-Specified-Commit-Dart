1d612ab5fab53c870f30cedbb8bbfa4000dd61df
Banee Ishaque K <baneeishaque@gmail.com>
2021-07-26 02:25:03 +0530

    Print Git Branches

--- bin/reset_all_branches_to_specified_commit.dart
+++ bin/reset_all_branches_to_specified_commit.dart
@@ -1,5 +1,8 @@
 import 'package:reset_all_branches_to_specified_commit/reset_all_branches_to_specified_commit.dart' as reset_all_branches_to_specified_commit;
 
 void main(List<String> arguments) {
-  print('Hello world: ${reset_all_branches_to_specified_commit.calculate()}!');
+  // print('Hello world: ${reset_all_branches_to_specified_commit.calculate()}!');
+  // reset_all_branches_to_specified_commit.printGitVersion();
+  reset_all_branches_to_specified_commit.printGitBranches('C:/Banee_Data/To_Sync/Lab_Banee/Azure_DevOps/VS2019-Azure-DevOps');
+  // reset_all_branches_to_specified_commit.printGitBranches('');
 }
--- lib/reset_all_branches_to_specified_commit.dart
+++ lib/reset_all_branches_to_specified_commit.dart
@@ -1,3 +1,18 @@
-int calculate() {
+import 'dart:io';
+
+int calculate() {  
   return 6 * 7;
+}
+
+void printGitVersion() async {
+  var result = await Process.run('git', ['--version']);
+  stdout.write(result.stdout);
+  stderr.write(result.stderr);
+}
+
+void printGitBranches(String repoDirectory) async {
+  var result = await Process.run('git', ['branch'], workingDirectory: repoDirectory);
+  // var result = await Process.run('git', ['branch']);
+  stdout.write(result.stdout);
+  stderr.write(result.stderr);
 }
